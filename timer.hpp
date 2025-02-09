763218b56cd57558bc405db3bf91b73b64e2208e
叶月枫 <yeyuefeng699@qq.com>
2025-02-09 11:46:33 +0800

    feat: Timer
    Add the cross-platform Timer for C++ to calculate the time that function takes

---
+++ timer.hpp
@@ -0,0 +1,105 @@
+/** 使用方法：
+ * 引用该头文件，在需要计时的函数内第一条语句前创建实例即可
+ *
+ *      #include "timer.hpp"
+ *
+ *      Timer timer("Timer");
+ *
+ * 该对象示例会在构建时自动初始化，析构时自动输出，不会影响原代码的运行时间
+ *
+ * 作者：Hazuki
+ * 2025-02-09
+ */
+
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+// 跨平台终端颜色控制
+class TerminalColor
+{
+public:
+#ifdef _WIN32
+    static int getInitialTextAttribute()
+    {
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        if (GetConsoleScreenBufferInfo(hStdout, &csbi))
+        {
+            return csbi.wAttributes;
+        }
+        else
+        {
+            std::cerr << "Error getting console screen buffer info." << std::endl;
+            return -1;
+        }
+    }
+
+    static void setGreen()
+    {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_GREEN);
+    }
+
+    static void reset()
+    {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), textAttribute_);
+    }
+
+    // 初始化静态成员变量
+    static int textAttribute_;
+#else
+    static void setGreen()
+    {
+        std::cout << "\033[1;32m" << std::flush;
+    }
+
+    static void reset()
+    {
+        std::cout << "\033[0m" << std::flush;
+    }
+#endif
+};
+
+// 定义静态成员变量
+#ifdef _WIN32
+int TerminalColor::textAttribute_ = TerminalColor::getInitialTextAttribute();
+#endif
+
+// 核心计时器
+class Timer
+{
+public:
+    // 输出的小数位数
+    static constexpr int PRECISION = 6;
+
+    Timer(const std::string &label = "Timer") : label_(label)
+    {
+        start_ = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Timer()
+    {
+        end_ = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_ - start_);
+
+        std::cout << "\nTime taken by " << label_ << " is ";
+        TerminalColor::setGreen();
+        std::cout << std::fixed << std::setprecision(PRECISION) << (double)duration.count() / 1000000;
+        TerminalColor::reset();
+        std::cout << " seconds." << std::endl;
+    }
+
+private:
+    std::chrono::high_resolution_clock::time_point start_;
+    std::chrono::high_resolution_clock::time_point end_;
+    std::string label_;
+};
+
+#endif
